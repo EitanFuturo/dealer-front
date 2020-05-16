b8044837a3e1594b8c17164648c4e74383012334
EitanFuturo <llovia@gmail.com>
2020-05-16 17:50:14 -0300

    Se completa el video 5.

--- src/axios/index.js
+++ src/axios/index.js
@@ -29,3 +29,28 @@ securedAxiosInstance.interceptors.request.use(config => {
 
   return config
 })
+
+securedAxiosInstance.interceptors.response.use(null, error => {
+  if ( error.response && error.response.config && error.response.status === 401 ){
+    return plainAxiosInstance.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf }})
+    .then(response => {
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+
+      let retryConfig = error.response.config
+      retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf
+      return plainAxiosInstance.request(retryConfig)
+    }).catch(error => {
+     delete localStorage.csrf
+     delete localStorage.signedIn
+
+     location.replace('/') 
+     return Promise.reject(error)
+    })
+  } else {
+    return Promise.reject(error)
+  }
+})
+
+export { securedAxiosInstance, plainAxiosInstance }
+
